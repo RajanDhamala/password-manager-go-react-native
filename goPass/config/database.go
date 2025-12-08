7c061b2d38294ed02e9d57e6a45c67b146b20d13
RajanDhamala <Rajandhamala0123@gmail.com>
2025-12-08 21:53:33 +0545

    initial setup

---
+++ goPass/config/database.go
@@ -0,0 +1,33 @@
+package config
+
+import (
+	"fmt"
+	"log"
+
+	"gorm.io/driver/postgres"
+	"gorm.io/gorm"
+)
+
+var DB *gorm.DB
+
+func ConnectDB() {
+	// Example credentials — replace with yours
+	host := "localhost"
+	user := "postgres"
+	password := "yourpassword"
+	dbname := "go_pass"
+	port := "5432"
+
+	dsn := fmt.Sprintf(
+		"host=%s user=%s password=%s dbname=%s port=%s sslmode=disable",
+		host, user, password, dbname, port,
+	)
+
+	database, err := gorm.Open(postgres.Open(dsn), &gorm.Config{})
+	if err != nil {
+		log.Fatal("Failed to connect to database:", err)
+	}
+
+	DB = database
+	fmt.Println(" Connected to PostgreSQL!")
+}
---
+++ goPass/main.go
@@ -0,0 +1,59 @@
+package main
+
+import (
+	"log"
+	"os"
+
+	"github.com/gofiber/fiber/v2"
+	"github.com/gofiber/fiber/v2/middleware/cors"
+	"github.com/gofiber/fiber/v2/middleware/logger"
+	"github.com/joho/godotenv"
+	"goPass/config"
+	"goPass/models"
+	"goPass/routes"
+)
+
+func main() {
+	err := godotenv.Load()
+	if err != nil {
+		log.Println("No .env file found, using system environment variables")
+	}
+	app := fiber.New()
+	app.Use(cors.New(cors.Config{
+		AllowOrigins:     "http://localhost:5173",
+		AllowHeaders:     "Origin, Content-Type, Accept",
+		AllowCredentials: true,
+	}))
+	app.Get("/robots.txt", func(c *fiber.Ctx) error {
+		return c.SendFile("./robots.txt")
+	})
+
+	app.Use(logger.New())
+	config.ConnectDB()
+	// Auto-create table
+	// config.DB.Migrator().DropTable(&models.Post{}, &models.User{})
+	// config.DB.AutoMigrate(&models.User{}, &models.Post{})
+
+	error := config.DB.AutoMigrate(
+		&models.AppUser{},    // parent
+		&models.Device{},     // child
+		&models.VaultEntry{}, // child
+	)
+	if error != nil {
+		log.Fatal("Migration failed:", err)
+	}
+
+	// Setup routes
+	router.UserRoute(app)
+	router.AppRouter(app)
+	router.DeviceRoute(app)
+	router.VaultRoute(app)
+	router.AuthRoute(app)
+
+	port := os.Getenv("PORT")
+	if port == "" {
+		port = "8080" // default port
+	}
+
+	app.Listen(":" + port)
+}
